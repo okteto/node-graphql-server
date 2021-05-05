3d31997206f51f819f6cbc1b9d9b294f18684f75
Ramiro Berrelleza <ramiro@okteto.com>
2021-05-04 21:47:19 -0700

    add redis to the stack

--- service/index.js
+++ service/index.js
@@ -1,5 +1,9 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
+const Redis = require("ioredis");
+
+const redis = new Redis(6379, "redis");
+const key = "books";
 
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
@@ -17,25 +21,35 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
-`;
 
-const books = [
-    {
-      id: '1',
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      id: '2',
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+  type Mutation {
+    addBook(id: [ID]!, title: String!, author: String!): Book!
+  }
+`;
 
   const resolvers = {
     Query: {
-      books: () => books,
+      books: () => {
+        const r = await redis.lrange(key, 0, -1);
+        const result = [];
+        r.forEach(b => {
+          console.log(r);
+        })
+
+        return result;
+      },
     },
+    Mutation: {
+      addBook: (id, title, author) => {
+        const book = {
+          id: id, 
+          title: title, 
+          author:author
+        };
+
+        return redis.rpush(key, book)
+      },
+    }
   };
 
   const server = new ApolloServer({
