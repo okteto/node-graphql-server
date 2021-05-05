c45956d53fad44fb858e324642d22ca0598cdae1
Ramiro Berrelleza <ramiro@okteto.com>
2021-05-04 22:32:29 -0700

    add redis and UUID

--- service/index.js
+++ service/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { buildFederatedSchema } = require('@apollo/federation');
+const { v4: uuidv4 } = require('uuid');
 const Redis = require("ioredis");
 
 const redis = new Redis(6379, "redis");
@@ -9,8 +10,8 @@ const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
-  type Book @key(fields: "id"){
-    id: ID!
+  type Book{
+    id: String
     title: String
     author: String
   }
@@ -23,31 +24,39 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    addBook(id: [ID]!, title: String!, author: String!): Book!
+    addBook(title: String!, author: String!): Book!
   }
 `;
 
   const resolvers = {
     Query: {
-      books: () => {
-        const r = await redis.lrange(key, 0, -1);
-        const result = [];
-        r.forEach(b => {
-          console.log(r);
-        })
-
-        return result;
+      books: async () => {
+        const keys = await redis.lrange("books", 0, -1);
+        
+        const books = [];
+        for (let i = 0; i < keys.length; i++) {
+          const key = keys[i];
+          const b = await redis.hgetall(key);
+          if (b.id) {
+            books.push(b);
+          }
+        }
+        
+        return books;
       },
     },
     Mutation: {
-      addBook: (id, title, author) => {
+      addBook: async (_, {title, author}, {dataSources}) => {
         const book = {
-          id: id, 
-          title: title, 
-          author:author
-        };
+          id: uuidv4(),
+          title: title,
+          author: author,
+        }
 
-        return redis.rpush(key, book)
+        await redis.hmset(`book:${book.id}`, book)
+        await redis.lpush("books", `book:${book.id}`)
+
+        return book;
       },
     }
   };
